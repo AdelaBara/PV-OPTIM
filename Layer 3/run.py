7e8c7b2978b5f44f3db7fc2370e913043e075195
AdelaBara <124293203+AdelaBara@users.noreply.github.com>
2023-02-04 20:13:42 +0200

    Delete run.py

--- Layer 3/run.py
+++
@@ -1,6 +0,0 @@
-from HR import app
-#Requirements: pip install flask, flask_sqlalchemy, flask_wtf, wtforms, WTForms-Alchemy, pymysql, flask_login, flask-bcrypt
-
-if __name__=='__main__':
-    app.run(debug=True)
-    #app.run(host='192.168.0.111', port='5000', debug=True)
